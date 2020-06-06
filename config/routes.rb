89e8042418cc0fcd2c25236ece3efcf4778a91c3
”suno” <stargazerastronomy2@gmail.com>
2020-06-06 03:53:10 +0000

    ソクラテス作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  #get 'users/new'
+  #get 'pages/index'
   root 'pages#index'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
